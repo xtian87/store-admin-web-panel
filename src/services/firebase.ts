da263e0495be272cbd5c55c6c7e0ea161e81bf47
Christian Martín Ramos Vega <xtian@wayra.com>
2025-08-30 08:39:58 -0500

    Delete content firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_et4nED-IM4KZswO6vxn_TrEIj27K3y4",
-  authDomain: "store-cadc8.firebaseapp.com",
-  projectId: "store-cadc8",
-  storageBucket: "store-cadc8.firebasestorage.app",
-  messagingSenderId: "626475566960",
-  appId: "1:626475566960:web:be35283665d2103bc9b1b5",
+  apiKey: "-",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "s",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
